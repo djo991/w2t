669aced183382d0fb89d87bd854c3eed75fc415d
Djordje Stojanovic <53429701+djo991@users.noreply.github.com>
2025-11-20 11:44:00 +0100

    Update signup.tsx remove label

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
 import { useState } from "react";
